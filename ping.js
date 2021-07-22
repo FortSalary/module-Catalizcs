4921369cb8c7ba5bb7f8d9aef1695fc87cd5960e
catalizcs <vanhuy69@icloud.com>
2021-07-23 00:50:18 +0700

    fix ping

--- ping.js
+++ ping.js
@@ -1,6 +1,6 @@
 module.exports.config = {
 	name: "ping",
-	version: "1.0.4",
+	version: "1.0.5",
 	hasPermssion: 1,
 	credits: "Mirai Team",
 	description: "tag toàn bộ thành viên",
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event, args }) {
 	try {
 		const botID = api.getCurrentUserID();
 		var listAFK, listUserID;
-		global.moduleData["afk"].afkList ? listAFK = Object.keys(global.moduleData["afk"].afkList) : listAFK = []; 
+		global.moduleData["afk"] && global.moduleData["afk"].afkList ? listAFK = Object.keys(global.moduleData["afk"].afkList || []) : listAFK = []; 
 		listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
 		listUserID = listUserID.filter(item => !listAFK.includes(item));
 		var body = (args.length != 0) ? args.join(" ") : "@everyone", mentions = [], index = 0;
